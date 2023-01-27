fc2dd53ad2d32df9de95bc4f7d6d491231a13932
nlambert18 <91149071+nlambert18@users.noreply.github.com>
2023-01-27 15:05:53 -0700

    css

--- chamber/scripts/head_foot.js
+++ chamber/scripts/head_foot.js
@@ -7,4 +7,13 @@ document.querySelector("#lastmodified").textContent = document.lastModified;
 
 //todays full date
 const fulldate = new Intl.DateTimeFormat("en-US", {dateStyle: "full"}).format(new Date());
-document.querySelector("#fulldate").textContent = fulldate.getDate();
+document.querySelector("#fulldate").textContent = fulldate;
+
+function toggleMenu(){
+    document.querySelector("#navbar").classList.toggle('menu-active');
+    document.querySelector("#menu-closed").classList.toggle('menu-active');
+    document.querySelector("#menu-open").classList.toggle('menu-active');
+}
+
+
+document.querySelector("#hamburger-menu").addEventListener('click', toggleMenu);
