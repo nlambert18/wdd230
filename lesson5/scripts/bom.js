e21b8f4052f0b796ebf3d6ef2ad80250a7ddf0be
nlambert18 <91149071+nlambert18@users.noreply.github.com>
2023-02-06 10:41:08 -0700

    Dom

---
+++ lesson5/scripts/bom.js
@@ -0,0 +1,43 @@
+// Create three variables that hold references to the input, button, and list elements using const.
+const chapterInput = document.querySelector("#favchap");
+const addButton = document.querySelector("main button");
+const chapterList = document.querySelector("#list");
+
+// Create an click event listener for the Add Chapter button using addEventListener and an anonymous function. 
+
+addButton.addEventListener('click', () => {
+
+// make sure the input is not blank before doing the following remaining tasks in this list
+if (chapterInput.value == ""){
+    return;
+}
+// li element
+let newListItem = document.createElement("li");
+
+// delete button
+let deleteButton = document.createElement("button");
+
+// populate the li elements textContent or innerHTML with the input
+newListItem.textContent = chapterInput.value;
+
+// populate the button textContent with an ❌
+deleteButton.textContent = '❌';
+
+// append the li element with the delete button
+newListItem.appendChild(deleteButton);
+
+// append the list element with the li element just created and appended with text and the delete button
+chapterList.appendChild(newListItem);
+
+// add an event listener to the delete button that removes the li element when clicked
+deleteButton.addEventListener('click', () => {
+    newListItem.remove();
+});
+
+// send the focus to the input element
+chapterInput.focus();
+
+// change the input value to nothing or the empty string to clean up the interface for the user
+chapterInput.value = "";
+});
+
