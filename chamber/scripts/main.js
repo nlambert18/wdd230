755f64f8502f7d64c64e1ec5835d3110ef54bcf1
nlambert18 <91149071+nlambert18@users.noreply.github.com>
2023-02-14 10:49:45 -0700

    change css, addbanner

---
+++ chamber/scripts/main.js
@@ -0,0 +1,6 @@
+// Show the meet-greet message on Mon/Tues
+// Date.getDay() uses a 0 based index to return the day of the week
+var messagedate = new Date();
+if (messagedate.getDay() == 1 || messagedate.getDay() == 2) {
+  document.querySelector("#meet-greet").classList.add("active");
+}
